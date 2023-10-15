cc252943dc597c7cb9590ec57facacf7634dacd1
ㅤㅤㅤ <64054647+skyallaround@users.noreply.github.com>
2023-10-15 04:01:37 +0000

    ye

--- browerscript.js
+++ browerscript.js
@@ -1,50 +1,35 @@
-function openSettings() {
-    window.open("settings.html", "_blank", "width=600,height=400");
-  }
+const isDarkMode = localStorage.getItem("darkMode") === "true";
 
-  const isDarkMode = localStorage.getItem("darkMode") === "true";
+if (isDarkMode) {
+  var linkElement = document.getElementById("stylesheet");
+  linkElement.href = "./darktabs.css";
+} else {
+  var linkElement = document.getElementById("stylesheet");
+  linkElement.href = "./lighttabs.css";
+}
 
-  if (isDarkMode) {
-    var linkElement = document.getElementById("stylesheet");
-    linkElement.href = "./darktabs.css";
-  } else {
-    var linkElement = document.getElementById("stylesheet");
-    linkElement.href = "./lighttabs.css";
-  }
-
-  function createNewTab() {
-    const tabsContainer = document.querySelector(".tabs");
-    const newTab = document.createElement("div");
-    newTab.classList.add("tab");
-    newTab.innerHTML = `
-    <div class="title">Tab ${tabsContainer.children.length + 1}</div>
+function createNewTab(Title, Cache) {
+  const tabsContainer = document.querySelector(".tabs");
+  const newTab = document.createElement("div");
+  newTab.classList.add("tab");
+  if (Cache == null) { Cache = `No Content` }
+  if (Title == null) { Title = `Tab ${tabsContainer.children.length + 1}` }
+  const nice = Cache;
+  Cache=null
+  newTab.innerHTML = `
+    <div class="title">${Title}</div>
     <div class="close">×</div>
   `;
-    tabsContainer.appendChild(newTab);
+  tabsContainer.appendChild(newTab);
+  newTab.addEventListener("click", () => {
     const tabs = document.querySelectorAll(".tab");
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", () => {
-        tabs.forEach((tab) => tab.classList.remove("active"));
-        tab.classList.add("active");
-      });
-    });
-    const closeButtons = document.querySelectorAll(".close");
-    closeButtons.forEach((closeButton) => {
-      closeButton.addEventListener("click", (event) => {
-        event.stopPropagation();
-        closeButton.parentElement.remove();
-      });
-    });
-  }
-
-  const tabs = document.querySelectorAll(".tab");
-  tabs.forEach((tab) => {
-    tab.addEventListener("click", () => {
-      tabs.forEach((tab) => tab.classList.remove("active"));
-      tab.classList.add("active");
-    });
+    tabs.forEach((tab) => tab.classList.remove("active"));;
+    newTab.classList.add("active");
+    var linkElement = document.getElementById("display");
+    linkElement.innerHTML = nice;
   });
-
+  const tabs = document.querySelectorAll(".tab");
+  tabs.forEach((tab) => tab.classList.remove("active"));
   const closeButtons = document.querySelectorAll(".close");
   closeButtons.forEach((closeButton) => {
     closeButton.addEventListener("click", (event) => {
@@ -52,3 +37,29 @@ function openSettings() {
       closeButton.parentElement.remove();
     });
   });
+}
+
+createNewTab();
+
+function openSettings() {
+  //window.open("settings.html", "_blank", "width=600,height=400");
+  createNewTab("Settings", `        <body>
+    <iframe id="iframe" src="settings.html"></iframe>
+  </body>`)
+}
+
+const tabs = document.querySelectorAll(".tab");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tab.classList.add("active");
+  });
+});
+
+const closeButtons = document.querySelectorAll(".close");
+closeButtons.forEach((closeButton) => {
+  closeButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    closeButton.parentElement.remove();
+  });
+});
